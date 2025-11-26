34d2e068a924fa46784790e54f9726ed62c8a825
Jarren James <jarrenjames13@gmail.com>
2025-11-26 17:00:51 +0800

    added enrollments maping and rendering in a scroll view container

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { showToast } from "@/utils/toast/toast";
 import { useFocusEffect } from "@react-navigation/native";
 import * as SecureStore from 'expo-secure-store';
 import { useCallback, useEffect, useState } from "react";
-import { ScrollView } from "react-native";
+import { Pressable, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -26,7 +26,7 @@ export default function Index() {
           fetchUser();
       }, []);
 
-  useFocusEffect (() => {
+  useFocusEffect (
     useCallback(() => {
       const fetchEnrollments = async () => {
         if (userId === null) return;
@@ -35,7 +35,7 @@ export default function Index() {
           const data = response.data;
           console.log('Fetched enrollments data:', data);
           if (response.status === 200){
-              setEnrollments(data);
+              setEnrollments(data.enrollments);
           }
           else if (response.status === 400 || response.status === 401 || response.status === 403) {
             showToast({
@@ -62,11 +62,63 @@ export default function Index() {
           });
         }
       }
-    },[])
-  })
+      fetchEnrollments();
+    },[userId])
+  )
+
+  const activeEnrollments = enrollments.filter(enrollment => {
+    const now = new Date();
+    const startDate = new Date(enrollment.start_date);
+    const endDate = new Date(enrollment.end_date);
+    return now >= startDate && now <= endDate;
+  });
+
+  const completedEnrollments = enrollments.filter(enrollment => {
+    const now = new Date();
+    const endDate = new Date(enrollment.end_date);
+    return now > endDate;
+  });
+
   return (
     <SafeAreaView className="flex-1 justify-center items-center">
-      <ScrollView>
+      <ScrollView className="w-full px-6 py-4 bg-gray-100">
+        <Text className="text-3xl font-bold text-black py-4 rounded-full px-3 mt-2 text-center">
+          Active Courses
+        </Text>
+        <View className="w-full py-8">
+          {activeEnrollments.length === 0 ? (
+            <Text className="text-base">No active enrollments.</Text>
+          ) : (
+            activeEnrollments.map((enrollment) => (
+              <View key={enrollment.enrollment_id} className="bg-white rounded-xl shadow-md mb-4 px-8 py-9">
+                <Text className="text-lg font-semibold text-gray-800">{enrollment.course_title}</Text>
+                <Text className="text-gray-600">Term: {enrollment.term_code}</Text>
+                <Text className="text-gray-600">Start Date: {new Date(enrollment.start_date).toLocaleDateString()}</Text>
+                <Text className="text-gray-600">End Date: {new Date(enrollment.end_date).toLocaleDateString()}</Text>
+                <Pressable className="mt-6 bg-blue-500 rounded-xl py-2 px-3 items-center">
+                  <Text className="text-white text-lg font-semibold">View Details</Text>
+                </Pressable>
+              </View>
+            ))
+          )}
+        </View>
+        <Text className="text-3xl font-bold text-black py-4 rounded-full px-3 textt-center mt-6">
+          Completed Courses
+        </Text>
+        <View className="w-full px-4 mb-6">
+          {completedEnrollments.length === 0 ? (
+            <Text className="text-base">No completed enrollments.</Text>
+          ) : (
+            completedEnrollments.map((enrollment) => (
+              <View key={enrollment.enrollment_id} className="bg-white rounded-xl shadow-md mb-4 px-8 py-9">
+                <Text className="text-lg font-semibold text-gray-800">{enrollment.course_title}</Text>
+                <Text className="text-gray-600">Term: {enrollment.term_code}</Text>
+                <Text className="text-gray-600">Start Date: {new Date(enrollment.start_date).toLocaleDateString()}</Text>
+                <Text className="text-gray-600">End Date: {new Date(enrollment.end_date).toLocaleDateString()}</Text>
+              </View>
+            ))
+          )}
+        </View>
 
       </ScrollView>
     </SafeAreaView>
