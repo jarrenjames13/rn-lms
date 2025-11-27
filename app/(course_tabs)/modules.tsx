706e94c105609fadcd6b2ade57cb959d15ed42f8
Jarren James <jarrenjames13@gmail.com>
2025-11-27 11:54:34 +0800

    initialized dummy screens for course

---
+++ app/(course_tabs)/modules.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function modules() {
+  return (
+    <View>
+      <Text>modules</Text>
+    </View>
+  );
+}
---
+++ app/(course_tabs)/quiz.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function quiz() {
+  return (
+    <View>
+      <Text>quiz</Text>
+    </View>
+  );
+}
