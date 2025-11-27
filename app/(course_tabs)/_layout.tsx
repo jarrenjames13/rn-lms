fb97db156755898a4b876f960a2ff54882f3f01f
Jarren James <jarrenjames13@gmail.com>
2025-11-27 13:21:55 +0800

    defined tab screens in course tab layout

--- app/(course_tabs)/_layout.tsx
+++ app/(course_tabs)/_layout.tsx
@@ -1,5 +1,50 @@
 import { Tabs } from "expo-router";
 
 export default function courseTabs() {
-  return <Tabs />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="overview"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Overview",
+        }}
+      />
+      <Tabs.Screen
+        name="modules"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Modules",
+        }}
+      />
+      <Tabs.Screen
+        name="quiz"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Quiz",
+        }}
+      />
+      <Tabs.Screen
+        name="exams"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Exams",
+        }}
+      />
+      <Tabs.Screen
+        name="grade"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Grade",
+        }}
+      />
+    </Tabs>
+  );
 }
