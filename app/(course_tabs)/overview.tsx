f08b1d95e7a58a1644b0ab81a92304276fff4702
Jarren James <jarrenjames13@gmail.com>
2025-11-28 09:25:10 +0800

    initializing course overview with stats and comments

--- app/(course_tabs)/overview.tsx
+++ app/(course_tabs)/overview.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import { Text, View } from "react-native";
 
 export default function Overview() {
-  const { course_id } = useCourseStore();
-  console.log("Current course_id in overview:", course_id);
+  const { course_id, instance_id } = useCourseStore();
   return (
     <View>
       <Text>Overview</Text>
