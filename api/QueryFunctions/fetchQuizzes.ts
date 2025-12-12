fa48616378b676e4d9ebb3d0aefb9fa322e914a7
jarren <jarrenjames13@gmail.com>
2025-12-12 16:51:07 +0800

    integrated student quizzes list types in queryfn

--- api/QueryFunctions/fetchQuizzes.ts
+++ api/QueryFunctions/fetchQuizzes.ts
@@ -1,9 +1,12 @@
+import { StudentQuizzes } from "@/types/api";
 import { getData } from "@/utils/fetcher";
 
 export const fetchQuizzes = async (courseId: number) => {
   try {
-    const response = await getData(`/modules/student-quizzes/${courseId}`);
-    const data = response.data;
+    const response = await getData<StudentQuizzes>(
+      `/modules/student-quizzes/${courseId}`
+    );
+    const data: StudentQuizzes = response.data;
     return data;
   } catch (error) {
     console.log("Error fetching quizzes:", error);
