9100e6ca0bc4a6cc37b4c974c688d508bbcc8c91
jarren <jarrenjames13@gmail.com>
2025-12-16 11:48:18 +0800

    created query function for the quiz questions with proper typesafety

---
+++ api/QueryFunctions/fetchQuizQuestions.ts
@@ -0,0 +1,22 @@
+import { QuestionsResponse } from "@/types/api";
+import { getData } from "@/utils/fetcher";
+
+export const fetchQuizQuestions = async (
+  quizId: number,
+  instanceId: number
+) => {
+  try {
+    const res = await getData<QuestionsResponse>(
+      `/modules/student-quiz-questions/${quizId}${instanceId}`,
+      {}
+    );
+    const data: QuestionsResponse = res.data;
+    return data;
+  } catch (error: any) {
+    throw new Error(
+      error?.response?.data?.detail ||
+        error.message ||
+        "Error fetching quiz questions"
+    );
+  }
+};
