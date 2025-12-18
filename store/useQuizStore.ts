9cba0c3601e7463084762473fc9339be08ab451d
jarren <jarrenjames13@gmail.com>
2025-12-18 10:50:58 +0800

    modified quiz store to store selectedanswers also

--- store/useQuizStore.ts
+++ store/useQuizStore.ts
@@ -1,10 +1,12 @@
+import { OptionKey } from "@/types/api";
 import { create } from "zustand";
-
 type QuizStore = {
   quiz_id: number;
   setQuizId: (quiz_id: number) => void;
   instance_id: number;
   setInstanceId: (instance_id: number) => void;
+  selectedAnswers: Record<number, OptionKey>;
+  setSelectedAnswers: (answers: Record<number, OptionKey>) => void;
 };
 
 export const useQuizStore = create<QuizStore>((set) => ({
@@ -12,4 +14,7 @@ export const useQuizStore = create<QuizStore>((set) => ({
   setQuizId: (newQuizId: number) => set({ quiz_id: newQuizId }),
   instance_id: 0,
   setInstanceId: (newInstanceId: number) => set({ instance_id: newInstanceId }),
+  selectedAnswers: {},
+  setSelectedAnswers: (answers: Record<number, OptionKey>) =>
+    set({ selectedAnswers: answers }),
 }));
