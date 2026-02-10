83f031f2f0bac71063dc231ecf69101ce479920f
jarren <jarrenjames13@gmail.com>
2026-02-10 11:45:31 +0800

    modified file name implemented pascalCasing

---
+++ api/QueryOptions/quizAnswersOption.ts
@@ -0,0 +1,19 @@
+import { QueryClient } from "@tanstack/react-query";
+import { postQuizAnswers } from "../QueryFunctions/postQuizAnswers";
+
+export type QuizSubmitResponse = {
+  success: boolean;
+  score: number;
+  correct_answers: number;
+  total_questions: number;
+};
+export default function createQuizAnswersOptions(queryClient: QueryClient) {
+  return {
+    mutationFn: postQuizAnswers,
+    onSuccess: async (data: QuizSubmitResponse) => {
+      await queryClient.invalidateQueries({ queryKey: ["list_quizzes"] });
+
+      return data;
+    },
+  };
+}
