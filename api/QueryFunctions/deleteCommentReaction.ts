b0ccab5e3eae3d29bdfa499c8deeb79b44f28bb8
jarren <jarrenjames13@gmail.com>
2026-02-20 17:07:24 +0800

    added type for delete reaction response

--- api/QueryFunctions/deleteCommentReaction.ts
+++ api/QueryFunctions/deleteCommentReaction.ts
@@ -1,9 +1,15 @@
 import { deleteData } from "@/utils/fetcher";
 
+export interface CommentReactionResponse {
+  success: boolean;
+  message: string;
+}
+
 export const deleteCommentReaction = async (comment_id: number) => {
   try {
     const response = await deleteData(`/comment-section/${comment_id}/react`);
-    return response.data;
+    const data: CommentReactionResponse = response.data;
+    return data;
   } catch (error: any) {
     throw new Error(
       error?.response?.data?.detail ||
